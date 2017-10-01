147cb60918a6933c0c84f44515801dfa08310115
André Pereira <ap080221063@gmail.com>
2017-10-01 01:48:24 +0100

    1st Commit

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,3 +1,5 @@
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.util.*
 
 interface Operation {
@@ -10,19 +12,19 @@ interface Operation {
 class Calculator:Operation{
 
     override fun divide(var1:Double, var2:Double): Double {
-        return var1/var2
+        return TwoDecPlaceDouble(var1/var2)
     }
 
     override fun multiply(var1:Double, var2:Double): Double {
-        return var1*var2
+        return TwoDecPlaceDouble(var1*var2)
     }
 
     override fun subtract(var1:Double, var2:Double): Double {
-        return var1-var2
+        return TwoDecPlaceDouble(var1-var2)
     }
 
     override fun sum(var1:Double, var2:Double): Double {
-        return var1+var2
+        return TwoDecPlaceDouble(var1+var2)
     }
 }
 
@@ -30,20 +32,24 @@ fun main(args : Array<String>){
 
     var calculator = Calculator()
 
-    var numb1 = (0.0..10.0).random()
-    var numb2 = (0.0..10.0).random()
+    var numb1 = TwoDecPlaceDouble((0.0..10.0).random())
+    var numb2 = TwoDecPlaceDouble((0.0..10.0).random())
 
-    println("randomly sum two numbers: ${numb1},${numb2} = ${calculator.sum(numb1,numb2)}")
-    println("randomly subtract two numbers: ${numb1},${numb2} = ${calculator.subtract(numb1,numb2)}")
+    println("sum two numbers: ${numb1},${numb2} = ${calculator.sum(numb1,numb2)}")
+    println("subtract two numbers: ${numb1},${numb2} = ${calculator.subtract(numb1,numb2)}")
 
     if(numb2==0.0){
         println("cannot perform division between numbers: ${numb1},${numb2}")
     }else {
-        println("randomly divide two numbers: ${numb1},${numb2} = ${calculator.divide(numb1,numb2)}")
+        println("divide two numbers: ${numb1},${numb2} = ${calculator.divide(numb1,numb2)}")
     }
 
-    println("randomly multiply two numbers: ${numb1},${numb2} = ${calculator.multiply(numb1,numb2)}")
+    println("multiply two numbers: ${numb1},${numb2} = ${calculator.multiply(numb1,numb2)}")
 
 }
 
 fun ClosedRange<Double>.random() = Random().nextDouble()
+
+fun TwoDecPlaceDouble (doubIn: Double) : Double{
+    return BigDecimal(doubIn).setScale(2,RoundingMode.CEILING).toDouble()
+}
